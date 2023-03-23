70b1cb600c114f963647f9deec0a50b9f036ace4
ADVENCHER4 <73076392+ADVENCHER4@users.noreply.github.com>
2023-03-23 20:24:14 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -1,26 +1,35 @@
 // Copyright 2021 NNTU-CS
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
-
+#include <string>
 template<typename T, int size>
 class TStack {
-private:
-    T arr[size];
+ private:
+    T *arr;
     int head;
-public:
-    TStack() :head(-1) {}
-    void Push(T val) {
-        if (IsFull()) throw std::string("full");
-        else arr[++head] = val;
+
+ public:
+    TStack() :head(-1) {
+       arr = new T[size];
     }
-    const T Pop() {
-        if (IsEmpty()) throw std::string("emp");
-        else return arr[head--];
+    void Push(T val) {
+        if (IsFull())
+            throw std::string("full");
+        else
+            arr[++head] = val;
+    }
+    const T& Pop() {
+        if (IsEmpty())
+            throw std::string("emp");
+        else
+            return arr[head--];
 
     }
-    const T Get() {
-        if (IsEmpty()) throw std::string("emp");
-        else return arr[head];
+    const T& Get() {
+        if (IsEmpty())
+            throw std::string("emp");
+        else
+            return arr[head];
     }
     bool IsFull() const {
         return head == size - 1;
